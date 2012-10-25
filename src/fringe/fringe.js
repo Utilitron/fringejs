5ea057d11d41cfb0304fc777c0fe1f0e98440429
Erik Ashcraft <eriktashcraft@hotmail.com>
2012-10-25 07:00:19 -0500

    Update src/fringe/fringe.js

--- src/fringe/fringe.js
+++ src/fringe/fringe.js
@@ -7,7 +7,7 @@
 
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
+ * freely, without restriction.
  * 
  * AUTHOR
  * ======
@@ -16,255 +16,163 @@
  * Created: 1/20/2012
  */
 
-/**
- * Class
- * 
- * The base class
- * This function provides the ground work for:
- *  - creating and extending classes
- *  - managing namespaces
- *	- implementing interfaces
- *	- linking class hierarchy through the .super function
- *  
- */
-var Class = function(argObject, source){
-	if ("package" in argObject)
-		_packageName = argObject["package"];
-	
-	if ("class" in argObject)
-		_class = argObject["class"];
-	
-	if ("extends" in argObject)
-		_parentClass = argObject["extends"];
-
-	// Require a package? For now, yes.
-	if (_packageName != null){
-		// The new class
-		var func = function (){
-			// Are we extending or instantiating?
-			if (!this.extending)
-				this.constructor.apply(this, arguments);
-
-		};
-
-		// Set the _parentClass' extending attribute to true
-		_parentClass.extending = true;
-		
-		// Set up the parent class of the constructor.
-		// This will be used to cascade through super constructors
-		source[_class].parentClass = new _parentClass;
-		
-		// Set up our class' prototype, constructor, and super
-		func.prototype = new _parentClass;
-		func.prototype.constructor = source[_class];
-		func.prototype.className = _class;
-		
-		// Set up our super class
-		func.prototype.super = function(){
-			var caller = arguments.callee.caller;
-			var superConstructor = caller.parentClass.constructor;
-
-			superConstructor.apply(this, arguments);
-		};
-		
-		// Add the attributes and methods of the source object to the new class
-		for (i in source){
-			if (i != _class)
-				func.prototype[i] = source[i];
-		}
-
-		// Add the class to the package
-		_packageName[_class] = func;
-		
-		// Remove the _parentClass' extending attribute
-		delete _parentClass.extending;
-
-	}
-};
-
-
-
-//------------------------------------------------------------------------
 
 /* base package */
-var fringe = window.fringe = {
-	version: "0.0.1"
-};
-
+var fringe = window.fringe = Object.create(Object.prototype, {
+	/**
+	 * version
+	 *  public final String
+	 */
+	version: { writable:false, configurable:false, value: "0.0.1" }
+});
+ 
 /* package */
 fringe.util = {};
-
-//Class ArrayList
-Class({package: fringe.util, class: 'ArrayList', extends: Object}, {
-	
+ 
+//ArrayList
+fringe.util.ArrayList = Object.create(Object.prototype, {
 	/**
 	 * Element collection
+	 * private final Array
 	 */
-	elements: [],
-	
+	elements: { writeable:false, configurable:false, enumerable:false,  value: [] },
+ 
 	/**
-	 * Constructor
-	 */
-	ArrayList: function(){
-		this.elements = new Array();
-	},
-	
-	/**
+	 * public getter Number
 	 * Returns the number of elements in this collection.
-	 */
-	size: function(){
-		return this.elements.length;
-	},
-	
+	 */  
+	size: { configurable:false, enumerable:false, get: function(){ return this.elements.length; } },
+ 
 	/**
+	 * public getter Boolean
 	 * Returns true if this collection contains no elements.
-	 */
-	isEmpty: function (){
-		return (this.elements.length === 0);
-	},
-	
+	 */  
+	isEmpty: { configurable:false, enumerable:false, get: function(){ return (this.elements.length > 0); } },
+ 
 	/**
+	 * public static Boolean
 	 * Returns true if this collection contains the specified element.
-	 */
-	contains: function(obj){
-		return (obj in this.elements);
-	},
-	
-	/**
+	 */  
+	 contains: { writeable:false, configurable:false, enumerable:false, value: function(obj){ return (obj in this.elements); } },
+
+	 /**
+	 * public static Boolean
 	 * Adds an element to this collection.
-	 */
-	add: function(obj){
-		this.elements.push(obj);
-	},
-
+	 */  
+	add: { writeable:false, configurable:false, enumerable:false, value: function(obj){ this.elements.push(obj); } },
+ 
 	/**
+	 * public static Boolean
 	 * Removes an element to this collection.
-	 */
-	remove: function(obj){
-		var index = this.elements.indexOf(obj);
-		if (index != -1){
-			this.elements.splice(index,1);
-		}
-	}
-	
-	/**
-	 * Return the element at this index.
-	 * /
-	get: function(index){
-		TODO
-	}*/
-});
-
-//Class AjaxUtil
-Class({package: fringe.util, class: 'AjaxUtil', extends: Object}, {
-	
-	/**
-	 * Constructor
-	 */
-	AjaxUtil: function(){
-	},
-	
-	/**
-	 * Ajax Object
-	 */
-	ajaxObj: null,
-
-	/**
-	 * Getter for the Ajax Object
-	 */
-	getAjaxObject: function() {
-		if (this.ajaxObj == null) {
-			// error testing
-			if (window.XMLHttpRequest) {
-				this.ajaxObj = new XMLHttpRequest();
-			} else if (window.ActiveXObject) {
-				this.ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+	 */  
+	remove: { writeable:false, configurable:false, enumerable:false, 
+			  value: function(obj){
+				  var index = this.elements.indexOf(obj);
+				  		if (index != -1){
+				  			this.elements.splice(index,1);
+				  		}
+			  }
 			}
-		}
-
-		return this.ajaxObj;
+});
+ 
+//AjaxUtil
+fringe.util.AjaxUtil = Object.create(Object.prototype, {
+	/**
+	 * public static Array
+	 */  
+	METHODS: { writeable:false, configurable:false, enumerable:false, value: ['GET', 'POST', 'PUT'] },
+	METHOD_GET: { writeable:false, configurable:false, enumerable:false, value: 'GET' },
+	METHOD_POST: { writeable:false, configurable:false, enumerable:false, value: 'POST' },
+	METHOD_PUT: { writeable:false, configurable:false, enumerable:false, value: 'PUT' },
+	
+	/**
+	 * Method String
+	 */
+	_method: { configurable:false, enumerable:false, value: 'GET' },
+	method: { configurable:false,
+			  get: function() { return this._method },
+			  set: function(value) {
+				  if (value in this.METHODS)
+					  this._method = value
+					  else
+						  throw new Error('!!');
+			  }
 	},
 	
 	/**
 	 * URL String
 	 */
-	url: null,
-
+	url: { configurable:false, value: null },
+	 
 	/**
-	 * URL: setter
+	 * Parameters
 	 */
-	setUrl: function(url) {
-		this.url = url;
-	},
-
-	/**
-	 * Method String
-	 */
-	method: 'GET',
-
-	/**
-	 * Method: setter
-	 */
-	setMethod: function(method) {
-		this.method = method;
-	},
-
-	params: null,
-
-	/**
-	 * Params: setter
-	 */
-	setParams: function(params) {
-		this.params = params;
-	},
+	 params: { configurable:false, value: null },
 	
 	/**
-	 * Ajax Send Request
-	 */
-	sendRequest: function() {
-		var ajaxObj = this.getAjaxObject();
-		if (ajaxObj != null && this.url != null
-			&& this.ajaxResponseHandler != null) {
-			ajaxObj.onreadystatechange = this.ajaxResponseHandler;
-			
-			ajaxObj.open(this.method, this.url, true);
-			
-			if (this.method == 'POST'){
-				//Send the proper header information along with the request
-				ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			}
-			
-			ajaxObj.send(this.params);
-		} else {
-			var errorMessage = "AJAX ERROR!";
-			if (ajaxObj == null) {
-				errorMessage += "\n" + "The ajax object is not initialized.";
-			}
-
-			if (this.url == null) {
-				errorMessage += "\n" + "The url is not set.";
-			}
-
-			if (this.ajaxResponseHandler == null) {
-				errorMessage += "\n" + "The response handler is not set.";
-			}
-
-			alert(errorMessage);
-		}
+	 * public getter Ajax Object
+	 * Returns the Ajax Object.
+	 */  
+	_ajaxObj: { configurable:false, enumerable:false, value: null },
+	ajaxObj: { configurable:false, enumerable:false,
+			   get: function(){
+				   if (this._ajaxObj == null){
+					   if (window.XMLHttpRequest)
+						   this._ajaxObj = new XMLHttpRequest();
+	  
+					   else if (window.ActiveXObject)
+						   this._ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+	  
+					   else
+						   throw new Error('The ajax object could not be initialized.');
+				   }
+				   
+				   return this._ajaxObj;
+			   }
 	},
-
+	 
 	/**
 	 * Ajax Response Handler
 	 */
-	ajaxResponseHandler: null,
-
+	_ajaxResponseHandler: { configurable:false, enumerable:false, value: null },
+	ajaxResponseHandler: { configurable:false, enumerable:false,
+						   get: function() { return this._ajaxResponseHandler },
+						   set: function(responseHandler) {
+								this._ajaxResponseHandler = function() {
+									if (this.readyState == 4 || this.readyState == 'complete')
+										responseHandler(this);
+									};
+						   }
+	},
+	  
 	/**
-	 * Setter for the Ajax Response Handler
+	 * Ajax Send Request
 	 */
-	setAjaxResponseHandler: function(responseHandler) {
-		this.ajaxResponseHandler = function() {
-			if (this.readyState == 4 || this.readyState == 'complete')
-				responseHandler(this);
-		};
-	}
+	sendRequest: { configurable:false, enumerable:false, 
+				   value: function() {
+						if (this.url != null && this.ajaxResponseHandler != null) {
+							this.ajaxObj.onreadystatechange = this.ajaxResponseHandler;
+						  
+							this.ajaxObj.open(this.method, this.url, true);
+						  
+							if (this.method == 'POST'){
+									//Send the proper header information along with the request
+								this.ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+							}
+						  
+							this.ajaxObj.send(this.params);
+						} else {
+							if (this.url == null) {
+								errorMessage += "\n" + "The url is not set.";
+							}
+						 
+							if (this.ajaxResponseHandler == null) {
+								errorMessage += "\n" + "The response handler is not set.";
+							}
+					 
+							throw new Error(errorMessage);
+						}
+					}
+				}
+	 
 });
