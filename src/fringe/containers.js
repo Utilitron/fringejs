899e837431a93d6db0a0663f611e9e9257290211
Erik Ashcraft <eriktashcraft@hotmail.com>
2012-10-26 13:06:34 -0500

    Update src/fringe/containers.js

--- src/fringe/containers.js
+++ src/fringe/containers.js
@@ -1,12 +1,12 @@
 /*******************************************************************
- * Copyright (C) 2011 Utilitron.net
+ * Copyright (C) 2012 Utilitron.net
  *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
  * arising from the use of this software.
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
+ * freely, without restriction.
  * 
  * AUTHOR
  * ======
