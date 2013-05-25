3e3730645cbc2584bd4d195c6316efb55216ce13
Erik Ashcraft <eriktashcraft@hotmail.com>
2013-05-24 19:49:07 -0500

    Update fringe.js

--- src/fringe/fringe.js
+++ src/fringe/fringe.js
@@ -19,162 +19,279 @@
 
 /* base package */
 var fringe = window.fringe = Object.create(Object.prototype, {
-	/**
-	 * version
-	 *  public final String
-	 */
-	version: { writable:false, configurable:false, value: "0.0.1" }
+    /**
+     * version
+     *  public final String
+     */
+    version: {
+        writable: false,
+        configurable: false,
+        value: "0.0.1"
+    }
 });
- 
+
 /* package */
 fringe.util = {};
- 
+
 //ArrayList
-fringe.util.ArrayList = function(){ this.elements = [] }
+fringe.util.ArrayList = function() {
+    "use strict";
+    this.elements = [];
+};
 fringe.util.ArrayList.prototype = Object.create(Object.prototype, {
-	/**
-	 * Element collection
-	 * private final Array
-	 */
-	elements: { writable:true, configurable:false, enumerable:false,  value: null },
- 
-	/**
-	 * public getter Number
-	 * Returns the number of elements in this collection.
-	 */  
-	size: { writable:false, configurable:false, enumerable:true, value: function(){ return this.elements.length; } },
- 
-	/**
-	 * public getter Boolean
-	 * Returns true if this collection contains no elements.
-	 */  
-	isEmpty: { writable:false, configurable:false, enumerable:true, value: function(){ return (this.elements.length > 0); } },
- 
-	/**
-	 * public static Boolean
-	 * Returns true if this collection contains the specified element.
-	 */  
-	 contains: { writable:false, configurable:false, enumerable:true, value: function(obj){ return (obj in this.elements); } },
+    /**
+     * Element collection
+     * private final Array
+     */
+    elements: {
+        writable: true,
+        configurable: false,
+        enumerable: false,
+        value: null
+    },
 
-	 /**
-	 * public static Boolean
-	 * Adds an element to this collection.
-	 */  
-	add: { writable:false, configurable:false, enumerable:true, value: function(obj){ this.elements.push(obj); } },
- 
-	/**
-	 * public static Boolean
-	 * Removes an element to this collection.
-	 */  
-	remove: { writable:false, configurable:false, enumerable:true, 
-			  value: function(obj){
-				  var index = this.elements.indexOf(obj);
-				  		if (index != -1){
-				  			this.elements.splice(index,1);
-				  		}
-			  }
-			}
+    /**
+     * public getter Number
+     * Returns the number of elements in this collection.
+     */
+    size: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function() {
+            "use strict";
+            return this.elements.length;
+        }
+    },
+
+    /**
+     * public getter Boolean
+     * Returns true if this collection contains no elements.
+     */
+    isEmpty: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function() {
+            "use strict";
+            return (this.elements.length > 0);
+        }
+    },
+
+    /**
+     * public static Boolean
+     * Returns true if this collection contains the specified element.
+     */
+    contains: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function(obj) {
+            "use strict";
+            return (this.elements.hasOwnProperty(obj));
+        }
+    },
+
+    /**
+     * public static Boolean
+     * Adds an element to this collection.
+     */
+    add: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function(obj) {
+            "use strict";
+            this.elements.push(obj);
+        }
+    },
+
+    /**
+     * public static Boolean
+     * Removes an element to this collection.
+     */
+    remove: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function(obj) {
+            "use strict";
+            var index = this.elements.indexOf(obj);
+            if (index !== -1) {
+                this.elements.splice(index, 1);
+            }
+        }
+    }
 });
- 
+
 //AjaxUtil
-fringe.util.AjaxUtil = function(){ }
+fringe.util.AjaxUtil = function() {
+    "use strict";
+};
 fringe.util.AjaxUtil.prototype = Object.create(Object.prototype, {
-	/**
-	 * public static Array
-	 */  
-	METHODS: { writable:false, configurable:false, enumerable:false, value: ['GET', 'POST', 'PUT'] },
-	METHOD_GET: { writable:false, configurable:false, enumerable:true, value: 'GET' },
-	METHOD_POST: { writable:false, configurable:false, enumerable:true, value: 'POST' },
-	METHOD_PUT: { writable:false, configurable:false, enumerable:true, value: 'PUT' },
+    /**
+     * public static Array
+     */
+    METHODS: {
+        writable: false,
+        configurable: false,
+        enumerable: false,
+        value: ['GET', 'POST', 'PUT']
+    },
+    METHOD_GET: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: 'GET'
+    },
+    METHOD_POST: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: 'POST'
+    },
+    METHOD_PUT: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: 'PUT'
+    },
 
-	/**
-	 * Method String
-	 */
-	_method: { writable:true, configurable:false, enumerable:false, value: 'GET' },
-	method: { configurable:false,
-			  get: function() { return this._method },
-			  set: function(value) {
-				  if (value in this.METHODS)
-					  this._method = value
-					  else
-						  throw new Error('!!');
-			  }
-	},
+    /**
+     * Method String
+     */
+    _method: {
+        writable: true,
+        configurable: false,
+        enumerable: false,
+        value: 'GET'
+    },
+    method: {
+        configurable: false,
+        get: function() {
+            "use strict";
+            return this._method;
+        },
+        set: function(value) {
+            "use strict";
+            var index = this.METHODS.indexOf(value);
+            if (index !== -1) {
+                this._method = value;
+            } else {
+                throw new Error('Method Not Found');
+            }
+        }
+    },
 
-	/**
-	 * URL String
-	 */
-	url: { writable:true, configurable:false, value: null },
+    /**
+     * URL String
+     */
+    url: {
+        writable: true,
+        configurable: false,
+        value: null
+    },
 
-	/**
-	 * Parameters
-	 */
-	 params: { writable:true, configurable:false, value: null },
+    /**
+     * Parameters
+     */
+    params: {
+        writable: true,
+        configurable: false,
+        value: null
+    },
 
-	/**
-	 * public getter Ajax Object
-	 * Returns the Ajax Object.
-	 */  
-	_ajaxObj: { writable:true, configurable:false, enumerable:false, value: null },
-	ajaxObj: { configurable:false, enumerable:false,
-			   get: function(){
-				   if (this._ajaxObj == null){
-					   if (window.XMLHttpRequest)
-						   this._ajaxObj = new XMLHttpRequest();
+    /**
+     * public getter Ajax Object
+     * Returns the Ajax Object.
+     */
+    _ajaxObj: {
+        writable: true,
+        configurable: false,
+        enumerable: false,
+        value: null
+    },
+    ajaxObj: {
+        configurable: false,
+        enumerable: false,
+        get: function() {
+            "use strict";
+            if (this._ajaxObj === null) {
+                if (window.XMLHttpRequest) {
+                    this._ajaxObj = new XMLHttpRequest();
+                } else if (window.ActiveXObject) {
+                    this._ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+                } else {
+                    throw new Error('The ajax object could not be initialized.');
+                }
+            }
+            return this._ajaxObj;
+        }
+    },
 
-					   else if (window.ActiveXObject)
-						   this._ajaxObj = new ActiveXObject("Microsoft.XMLHTTP");
+    /**
+     * Ajax Response Handler
+     */
+    _ajaxResponseHandler: {
+        writable: true,
+        configurable: false,
+        enumerable: false,
+        value: null
+    },
+    ajaxResponseHandler: {
+        configurable: false,
+        enumerable: false,
+        get: function() {
+            "use strict";
+            return this._ajaxResponseHandler;
+        },
+        set: function(responseHandler) {
+            "use strict";
+            this._ajaxResponseHandler = function() {
+                if (this.readyState === 4 || this.readyState === 'complete') {
+                    responseHandler(this);
+                }
+            };
+        }
+    },
 
-					   else
-						   throw new Error('The ajax object could not be initialized.');
-				   }
+    /**
+     * Ajax Send Request
+     */
+    sendRequest: {
+        writable: false,
+        configurable: false,
+        enumerable: true,
+        value: function() {
+            "use strict";
+            var url = this.url, errorMessage = "";
+            if (this.url !== null && this.ajaxResponseHandler !== null) {
+                this.ajaxObj.onreadystatechange = this.ajaxResponseHandler;
 
-				   return this._ajaxObj;
-			   }
-	},
+                if (this.method === 'GET') {
+                    url += '?' + this.params;
+                }
 
-	/**
-	 * Ajax Response Handler
-	 */
-	_ajaxResponseHandler: { writable:true, configurable:false, enumerable:false, value: null },
-	ajaxResponseHandler: { configurable:false, enumerable:false,
-						   get: function() { return this._ajaxResponseHandler },
-						   set: function(responseHandler) {
-								this._ajaxResponseHandler = function() {
-									if (this.readyState == 4 || this.readyState == 'complete')
-										responseHandler(this);
-									};
-						   }
-	},
+                this.ajaxObj.open(this.method, url, true);
 
-	/**
-	 * Ajax Send Request
-	 */
-	sendRequest: { writable:false, configurable:false, enumerable:true, 
-				   value: function() {
-						if (this.url != null && this.ajaxResponseHandler != null) {
-							this.ajaxObj.onreadystatechange = this.ajaxResponseHandler;
+                if (this.method === 'POST') {
+                    //Send the proper header information along with the request
+                    this.ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                }
 
-							this.ajaxObj.open(this.method, this.url, true);
+                this.ajaxObj.send(this.params);
 
-							if (this.method == 'POST'){
-									//Send the proper header information along with the request
-								this.ajaxObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-							}
+            } else {
+                if (this.url === null) {
+                    errorMessage += "\n" + "The url is not set.";
+                }
 
-							this.ajaxObj.send(this.params);
-						} else {
-							if (this.url == null) {
-								errorMessage += "\n" + "The url is not set.";
-							}
+                if (this.ajaxResponseHandler === null) {
+                    errorMessage += "\n" + "The response handler is not set.";
+                }
 
-							if (this.ajaxResponseHandler == null) {
-								errorMessage += "\n" + "The response handler is not set.";
-							}
-
-							throw new Error(errorMessage);
-						}
-					}
-				}
-
+                throw new Error(errorMessage);
+            }
+        }
+    }
 });
